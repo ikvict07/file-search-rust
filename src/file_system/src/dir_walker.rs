8a505145bd7b3347af2444e3312f05dcb0878560
Anton <antongorobec101@gmail.com>
2024-04-13 17:50:11 +0200

    Azure integration added

--- src/file_system/src/dir_walker.rs
+++ src/file_system/src/dir_walker.rs
@@ -1,7 +1,16 @@
-use std::fs;
+use std::{fs, time};
+use std::error::Error;
 use std::io;
+use std::io::ErrorKind;
 use std::sync::{Arc, Mutex};
 use std::thread;
+use governor::{Quota, RateLimiter};
+use std::num::NonZeroU32;
+use futures::stream;
+use futures::stream::StreamExt;
+use serde_json::Value;
+use img_azure::azure_api::*;
+
 
 pub struct DirWalker {
     dirs: Arc<Mutex<Vec<String>>>,
@@ -78,7 +87,7 @@ impl DirWalker {
                         }
                     };
 
-                    match fs::read_dir(&dir) {
+                    match std::fs::read_dir(&dir) {
                         Ok(entries) => {
                             for entry in entries {
                                 match entry {
@@ -108,5 +117,54 @@ impl DirWalker {
         for handle in handles {
             handle.join().unwrap();
         }
+    }
+
+
+
+    pub async fn send_requests_for_dir_apply(&self, requests_per_sec: u32, f: impl Fn(Result<AzureResponse, ErrorKind>)) -> Result<(), Box<dyn Error>>{
+
+        let dir = self.dirs.lock().unwrap().pop().unwrap();
+
+        let mut dir_entries = tokio::fs::read_dir(dir).await?;
+
+        // Convert the ReadDir to a Stream
+        let dir_entries_stream = stream::unfold(dir_entries, |mut dir_entries| async {
+            match dir_entries.next_entry().await {
+                Ok(Some(entry)) => Some((entry, dir_entries)),
+                _ => None,
+            }
+        });
+
+        let callback = Arc::new(f);
+
+        println!("Starting processing");
+        let current = time::Instant::now();
+        let limiter = Arc::new(RateLimiter::direct(
+            Quota::per_second(NonZeroU32::new(requests_per_sec).unwrap())
+        ));
+
+        dir_entries_stream.for_each_concurrent(None, |entry| {
+            let limiter = Arc::clone(&limiter);
+
+            let callback = Arc::clone(&callback);
+            async move {
+                limiter.until_ready().await;
+                let path = entry.path();
+                if path.is_file() {
+                    // Wait until a permit is available
+
+                    let mut request = AzureRequest::new("4d7bd39a70c249eebd19f5b8d62f5d7b", vec!["tags", "caption"]);
+                    request.set_img(path.to_str().unwrap()).unwrap();
+
+                    let response= request.send_request().await.unwrap();
+                    let response_copy = response.json::<Value>().await.unwrap();
+                    let response_struct: Result<AzureResponse, ErrorKind> = AzureResponse::try_from(response_copy.clone());
+                    callback(response_struct);
+                }
+            }
+        }).await;
+
+        println!("Time elapsed: {:?}", current.elapsed());
+        Ok(())
     }
 }
---
+++ src/img_azure/src/azure_api.rs
@@ -0,0 +1,109 @@
+use std::error::Error;
+use std::io::{ErrorKind, Read};
+use reqwest::{Response};
+use serde::Deserialize;
+use serde_json::Value;
+
+pub struct AzureRequest {
+    client: reqwest::Client,
+    headers: reqwest::header::HeaderMap,
+    img: Vec<u8>,
+    request_adress: String,
+}
+
+impl AzureRequest {
+    pub fn new(key: &str, args: Vec<&str>) -> Self {
+        let client = reqwest::Client::new();
+        let mut headers = reqwest::header::HeaderMap::new();
+        headers.insert("Ocp-Apim-Subscription-Key", reqwest::header::HeaderValue::from_str(key).unwrap());
+        headers.insert("Content-Type", reqwest::header::HeaderValue::from_str("application/octet-stream").unwrap());
+
+        AzureRequest {
+            client,
+            headers,
+            img: Vec::new(),
+            request_adress: "https://file-search-rust-paid.cognitiveservices.azure.com/computervision/imageanalysis:analyze?api-version=2024-02-01&features=".to_string() + args.join(",").as_str(),
+        }
+    }
+
+    pub fn set_img(&mut self, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+        match std::fs::File::open(path)?.read_to_end(&mut self.img) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(Box::new(e)),
+        }
+    }
+
+    pub async fn send_request(&self) -> Result<Response, Box<dyn std::error::Error>> {
+        let response = self.client.post(&self.request_adress)
+            .headers(self.headers.clone())
+            .body(self.img.clone())
+            .send()
+            .await;
+        match response {
+            Ok(response) => Ok(response),
+            Err(e) => Err(Box::new(e)),
+        }
+    }
+}
+#[derive(Debug, Deserialize)]
+pub struct Label {
+    name: String,
+    score: f64,
+}
+impl Label {
+    pub fn new(name: String, score: f64) -> Self {
+        Label {
+            name,
+            score,
+        }
+    }
+}
+
+impl From<&Value> for Label {
+    fn from(value: &Value) -> Self {
+        let name = value["name"].as_str().unwrap().to_string();
+        let score = value["confidence"].as_f64().unwrap();
+        Label {
+            name,
+            score,
+        }
+    }
+}
+#[derive(Debug, Deserialize)]
+pub struct AzureResponse {
+    pub caption: String,
+    pub labels: Vec<Label>,
+}
+
+impl TryFrom<Value> for AzureResponse {
+    type Error = ErrorKind;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        let caption = value["captionResult"]["text"].as_str();
+
+        if caption.is_none() {
+            println!("No caption found");
+            println!("{:?}", value);
+            return Err(Self::Error::from(ErrorKind::NotFound));
+        }
+        let caption = caption.unwrap().to_string();
+        let mut labels = Vec::new();
+
+
+        let label = value["tagsResult"].get("values");
+        if label.is_none() {
+            println!("No labels found");
+            return Err(Self::Error::from(ErrorKind::NotFound));
+        }
+        let label_it = label.unwrap().as_array().unwrap();
+
+        for label in label_it {
+            labels.push(Label::new(label.get("name").unwrap().as_str().unwrap().to_string(), label.get("confidence").unwrap().as_f64().unwrap()));
+        }
+
+        Ok(AzureResponse {
+            caption,
+            labels,
+        })
+    }
+}
