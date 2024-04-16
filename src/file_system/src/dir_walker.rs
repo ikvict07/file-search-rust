9ce410186387bbda376bcd57c47074f3471dbee6
Anton <antongorobec101@gmail.com>
2024-04-16 23:08:01 +0200

    DirWalker fixes

--- src/file_system/src/dir_walker.rs
+++ src/file_system/src/dir_walker.rs
@@ -12,7 +12,8 @@ use futures::stream::StreamExt;
 use serde_json::Value;
 use img_azure::azure_api::*;
 use db::database::Database;
-
+use db::image;
+use im::GenericImageView;
 pub struct DirWalker {
     dirs: Arc<Mutex<Vec<String>>>,
 }
@@ -121,74 +122,107 @@ impl DirWalker {
     }
 
 
-    pub async fn send_requests_for_dir_apply(&self, db: Arc<Mutex<Option<Database>>>, requests_per_sec: u32, f: impl Fn(Result<AzureResponse, ErrorKind>, PathBuf) + Send + 'static) -> Result<(), Box<dyn Error>> {
+    pub async fn send_requests_for_dir_apply(&self, db: Arc<Mutex<Option<Database>>>, requests_per_sec: u32, f: impl Fn(Result<AzureResponse, ErrorKind>, PathBuf) + Send + Sync + 'static) -> Result<(), Box<dyn Error>> {
         let dirs = self.dirs.clone();
         let dirs_arc = Arc::clone(&dirs);
         let current = time::Instant::now();
         let f = Arc::new(f);
         let db = db.clone();
-        while let Some(dir) = {
-            let mut dirs = dirs.lock().unwrap();
-            dirs.pop()
-        } {
-            let dir_entries = tokio::fs::read_dir(dir).await?;
+        let mut handles = vec![];
+        for _ in 0..num_cpus::get() {
+            let dirs = Arc::clone(&dirs);
+            let f = Arc::clone(&f);
+            let db = Arc::clone(&db);
+            let dirs_arc = Arc::clone(&dirs_arc);
+            let handle = tokio::spawn(async move {
+                'inner: while let Some(dir) = {
+                    let dirs = dirs.lock();
+                    if dirs.is_err() {
+                        break 'inner;
+                    }
+                    let mut dirs = dirs.unwrap();
+                    dirs.pop()
+                } {
+                    let dir_entries = tokio::fs::read_dir(dir).await;
+                    if dir_entries.is_err() {
+                        continue;
+                    }
+                    let dir_entries = dir_entries.unwrap();
 
-            let dir_entries_stream = stream::unfold(dir_entries, |mut dir_entries| async {
-                match dir_entries.next_entry().await {
-                    Ok(Some(entry)) => Some((entry, dir_entries)),
-                    _ => None,
+                    let dir_entries_stream = stream::unfold(dir_entries, |mut dir_entries| async {
+                        match dir_entries.next_entry().await {
+                            Ok(Some(entry)) => Some((entry, dir_entries)),
+                            _ => None,
+                        }
+                    });
+
+                    let callback = f.clone();
+                    let limiter = Arc::new(RateLimiter::direct(
+                        Quota::per_second(NonZeroU32::new(requests_per_sec).unwrap()),
+                    ));
+
+                    let db = db.clone();
+                    let dirs_arc_clone = Arc::clone(&dirs_arc);
+
+                    dir_entries_stream.for_each_concurrent(None, move |entry| {
+                        let limiter = Arc::clone(&limiter);
+                        let callback = Arc::clone(&callback);
+                        let dirs_arc_clone = Arc::clone(&dirs_arc_clone);
+                        let db = Arc::clone(&db);
+                        let mut db = db.lock().unwrap();
+                        let path = entry.path();
+                        let mut flag = false;
+                        if (path.is_file()) {
+                            if is_image(path.to_str().unwrap()) {
+                                let img = im::open(path.to_str().unwrap());
+                                if img.is_err() {
+                                    println!("skip{}", path.to_str().unwrap());
+                                    flag = true;
+                                } else {
+                                    let img = img.unwrap();
+                                    let (width, height) = img.dimensions();
+                                    if width < 50 || height < 50 || width > 16000 || height > 16000 {
+                                        flag = true;
+                                    } else {
+                                        let db = db.as_mut().unwrap();
+                                        flag = db.exists_image_by_path(path.to_str().unwrap()).unwrap();
+                                    }
+                                }
+                            }
+                        }
+                        async move {
+                            if flag {
+                                return;
+                            }
+                            let path = entry.path();
+                            if path.is_file() {
+                                if !is_image(path.to_str().unwrap()) {
+                                    return;
+                                }
+                                let path_str = path.to_str().unwrap();
+
+
+                                limiter.until_ready().await;
+
+                                let mut request = AzureRequest::new("4d7bd39a70c249eebd19f5b8d62f5d7b", vec!["tags", "caption"]);
+                                request.set_img(path_str).unwrap();
+                                let response = request.send_request().await.unwrap();
+                                let response_copy = response.json::<Value>().await.unwrap();
+                                let response_struct: Result<AzureResponse, ErrorKind> = AzureResponse::try_from(response_copy.clone());
+
+                                callback(response_struct, path);
+                            } else if path.is_dir() {
+                                let mut dirs = dirs_arc_clone.lock().unwrap();
+                                dirs.push(path.to_str().unwrap().to_string());
+                            }
+                        }
+                    }).await;
                 }
             });
-
-            let callback = f.clone();
-            let limiter = Arc::new(RateLimiter::direct(
-                Quota::per_second(NonZeroU32::new(requests_per_sec).unwrap()),
-            ));
-
-            let db = db.clone();
-            let dirs_arc_clone = Arc::clone(&dirs_arc);
-
-            dir_entries_stream.for_each_concurrent(None, move |entry| {
-                let limiter = Arc::clone(&limiter);
-                let callback = Arc::clone(&callback);
-                let dirs_arc_clone = Arc::clone(&dirs_arc_clone);
-                let db = Arc::clone(&db);
-                let mut db = db.lock().unwrap();
-                let path = entry.path();
-                let mut flag = false;
-                if (path.is_file()) {
-                    if is_image(path.to_str().unwrap()) {
-                        let db = db.as_mut().unwrap();
-                        flag = db.exists_image_by_path(path.to_str().unwrap()).unwrap();
-                    }
-                }
-                async move {
-                    if flag {
-                        return;
-                    }
-                    let path = entry.path();
-                    if path.is_file() {
-                        if !is_image(path.to_str().unwrap()) {
-                            return;
-                        }
-                        let path_str = path.to_str().unwrap();
-
-
-                        limiter.until_ready().await;
-
-                        let mut request = AzureRequest::new("4d7bd39a70c249eebd19f5b8d62f5d7b", vec!["tags", "caption"]);
-                        request.set_img(path_str).unwrap();
-                        let response = request.send_request().await.unwrap();
-                        let response_copy = response.json::<Value>().await.unwrap();
-                        let response_struct: Result<AzureResponse, ErrorKind> = AzureResponse::try_from(response_copy.clone());
-
-                        callback(response_struct, path);
-                    } else if path.is_dir() {
-                        let mut dirs = dirs_arc_clone.lock().unwrap();
-                        dirs.push(path.to_str().unwrap().to_string());
-                    }
-                }
-            }).await;
+            handles.push(handle);
+        }
+        for handle in handles {
+            handle.await?;
         }
 
         println!("Time elapsed: {:?}", current.elapsed());
